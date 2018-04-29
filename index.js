9af2a70191edc815470e69effd5ad83d373415f8
Anuj Pandey <pandey.anujk@gmail.com>
2018-04-29 03:26:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`;
 }
 
-function happyHolidaysTo (holidayname, name) {
+function happyHolidayTo (holidayname, name) {
   return `Happy ${holidayname}, ${name}!`;
 }
 
